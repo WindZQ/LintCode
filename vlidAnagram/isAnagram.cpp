672f862faf64f33cd2b2a6e591cff76a3569911e
WindZQ <1049498972@qq.com>
2020-12-27 21:14:00 +0800

    The Vlid Anagram Problem Solution

---
+++ vlidAnagram/isAnagram.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <algorithm>
+
+class Solution
+{
+public:
+    bool isAnagram(std::string &s, std::string &t)
+    {
+        std::sort(s.begin(), s.end());
+        std::sort(t.begin(), t.end());
+        return s == t;
+    }
+};
