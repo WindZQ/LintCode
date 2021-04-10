edb0614b1bd09dc8193363d8a5ba4cd6d8fcdbf1
WindZQ <1049498972@qq.com>
2021-04-10 21:17:26 +0800

    The BaseBall Game Problem Solution

---
+++ baseBallGame/calPoints.cpp
@@ -0,0 +1,67 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+#define method1 1
+#define method2 0
+
+class Solution
+{
+public:
+#if method1
+    int calPoints(std::vector<std::string> &ops)
+    {
+        int ar[1010], pointer = -1, sum = 0;
+        for(std::string st : ops)
+        {
+            char c = st[0];
+            if(c == '+')
+            {
+                sum += (ar[++pointer] = ar[pointer - 1] + ar[pointer - 2]);
+            }
+            else if(c == 'D')
+            {
+                sum += (ar[++pointer] = 2 * ar[pointer - 1]);
+            }
+            else if(c == 'D')
+            {
+                sum -= ar[pointer--];
+            }
+            else
+            {
+                sum += (ar[++pointer] = std::stoi(st));
+            }
+        }
+
+        return sum;
+    }
+#endif
+
+#if method2
+    int calPoints(std::vector<std::string> &ops)
+    {
+        std::vector<int> v;
+        for(std::string op : ops)
+        {
+            if(op == '+')
+            {
+                v.push_back(v.back() + v[v.size() - 2]);
+            }
+            else if(op == 'D')
+            {
+                v.push_back(2 * v.back());
+            }
+            else if(op == 'C')
+            {
+                v.pop_back();
+            }
+            else
+            {
+                v.push_back(std::stoi(op));
+            }
+        }
+
+        return std:accumulate(v.begin(), v.end(), 0);
+    }
+#endif
+};
